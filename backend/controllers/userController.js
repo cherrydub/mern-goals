2d56d777dd5c97d16227d22cbc87f8143c24683c
cherrydub <cherrydub@gmail.com>
2023-12-11 15:53:11 +0000

    added middleware for having to have jwt token, with bearer in header in specific routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,87 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 // @desc    Auth a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
-  res.json({ messsage: "login user" });
-};
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  //check that user email exists
+  const user = await User.findOne({ email });
+
+  //now we compare the plain password we got from frontend, with the bcrypted pw in the db
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid creds");
+  }
+});
 
 // @desc    register new user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
-  res.json({ messsage: "registered user" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  //check if user already exists
+  if (userExists) {
+    res.status(400);
+    throw new Error("user already exists");
+  }
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  console.log("user here", user);
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  //   res.json({ messsage: "registered user" });
+});
 
 // @desc    get user data
 // @route   GET /api/users/me
-// @access  Public
-const getMe = (req, res) => {
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
   res.json({ messsage: "user data received" });
-};
+});
+
+//Generate JTW
+function generateToken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+}
 
 module.exports = { loginUser, registerUser, getMe };
